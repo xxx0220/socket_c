a0d6e815627d77fc7feea00208c130433fbf7635
xxx0220 <93422513+xxx0220@users.noreply.github.com>
2023-09-02 20:57:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 服务器地址和端口号
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8000;
+
+        // 读取txt2内容
+        //文件地址 "E:\my\socket\2\txt2.txt" 请修改为自己的文件地址
+        string sendData = File.ReadAllText("E:/my/socket/2/txt2.txt");
+        // Console.WriteLine("send data2 from c#: ");
+        // Console.WriteLine(sendData);
+
+        // 创建TCP监听器并开始监听连接请求
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+
+        Console.WriteLine("Waiting for client...");
+
+        // 等待客户端连接
+        TcpClient client = listener.AcceptTcpClient();
+        Console.WriteLine("Client connected");
+
+        // 获取与客户端通信的网络流
+        NetworkStream stream = client.GetStream();
+
+        // 读取客户端发送的数据txt1
+        byte[] buffer_rev = new byte[1024];
+        int bytesRead = stream.Read(buffer_rev, 0, buffer_rev.Length);
+        string receivedData = Encoding.ASCII.GetString(buffer_rev, 0, bytesRead);
+        Console.WriteLine("Received data from client: " + receivedData);
+
+        // 发送txt2文本文件内容给C++
+        byte[] buffer = Encoding.ASCII.GetBytes(sendData);
+        stream.Write(buffer, 0, buffer.Length);
+        
+        Console.WriteLine("Sent data to client: " + sendData);
+
+        // 关闭网络流和客户端连接
+        stream.Close();
+        client.Close();
+        listener.Stop();
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
---
+++ test1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+
+    //读取txt1文件
+    //文件地址 "E:\my\socket\1\txt1.txt" 请修改为自己的文件地址
+    std::ifstream file("E:/my/socket/1/txt1.txt");
+    std::string sendData((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+    
+    std::cout << "send data1 from c++ :"  << std::endl;
+    std::cout << sendData << std::endl;
+
+    // 初始化Winsock库
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "Failed to initialize Winsock" << std::endl;
+        return 1;
+    }
+
+    // 创建Socket
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Failed to create socket" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // 设置服务器的IP地址和端口号
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8000);
+    inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr));
+
+    // 连接到服务器
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cout << "Failed to connect to server" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // 发送数据到服务器
+    if (send(clientSocket, sendData.c_str(), sendData.size(), 0) == SOCKET_ERROR) {
+        std::cout << "Failed to send data to server" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // 接收来自服务器的响应数据
+    char buffer[1024]{};
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead == SOCKET_ERROR) {
+        std::cout << "Failed to receive data from server" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Received data from server: " << buffer << std::endl;
+    
+
+    // 清理和关闭Socket
+    closesocket(clientSocket);
+
+    // 关闭Winsock库
+    WSACleanup();
+    system("Pause");
+    return 0;
+}
